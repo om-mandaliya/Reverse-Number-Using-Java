30ce2e4f0e2fa46da469151d089295a5eb0d7f42
om mandaliya <ommandaliya6@gmail.com>
2024-12-18 17:39:23 +0530

    Add files via upload

---
+++ index.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        
+        int reverse = 0;
+        
+        while (num != 0) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num /= 10;
+        }
+        
+        System.out.println("Reversed number: " + reverse);
+        
+        scanner.close();
+    }
+}
